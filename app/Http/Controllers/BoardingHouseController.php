a7fbf755013ce333720b9852d00b878d3b27634f
bagus-net <bagussetyawan551@gmail.com>
2025-03-14 11:04:57 +0700

    front-findkos

--- app/Http/Controllers/BoardingHouseController.php
+++ app/Http/Controllers/BoardingHouseController.php
@@ -5,11 +5,9 @@ namespace App\Http\Controllers;
 use App\Interfaces\BoardingHouseRepositoryInterface;
 use App\Interfaces\CategoryRepositoryInterface;
 use App\Interfaces\CityRepositoryInterface;
-use App\Models\BoardingHouse;
-use App\Models\Category;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class BoardingHouseController extends Controller
 {
     private CityRepositoryInterface $cityRepository;
     private CategoryRepositoryInterface $categoryRepository;
@@ -25,11 +23,12 @@ class HomeController extends Controller
         $this->boardingHouseRepository = $boardingHouseRepository;
     }
 
-    public function index()  {
+    public function find()
+    {
+
         $categories = $this->categoryRepository->getAllCategories();
-        $popularBoardingHouses = $this->boardingHouseRepository->getPopularBoardingHouses();
         $cities = $this->cityRepository->getAllCities();
-        $boardingHouses = $this->boardingHouseRepository->getAllBoardingHouses();
-        return view ('pages.home', compact('categories','popularBoardingHouses', 'cities', 'boardingHouses'));
+
+        return view('pages.boarding-house.find',compact('categories','cities'));
     }
 }
